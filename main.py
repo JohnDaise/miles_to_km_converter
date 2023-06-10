c6d97d46ed6965de9d402e2968db7d86b9cef7dd
JohnDaise <jdaise@gmail.com>
2023-06-10 11:14:16 -0500

    setup window

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+def calculate_conversion():
+    pass
+
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+
+
+window.mainloop()
